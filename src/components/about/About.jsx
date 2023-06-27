574f1712d66a8440315221e2a11ac8845a93feb7
Ha_Tan_Luc <hatanluc0@outlook.fr>
2023-06-27 17:50:47 +0200

    Second commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {PiStudentBold} from 'react-icons/pi'
+import {FiUsers} from 'react-icons/fi'
+import {BiFolder} from 'react-icons/bi'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <PiStudentBold className='about__icon'/>
+              <h5>Experience</h5>
+              <small> Junior Developpeur</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>no one</small>
+            </article>
+
+            <article className="about__card">
+              <BiFolder className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ completed</small>
+            </article>
+            
+          </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius doloribus, facere sequi commodi esse consequatur nisi fugit obcaecati numquam quod fugiat. Sed, voluptatem facilis illum provident a harum veniam natus.
+            </p>
+            <a href="#contact" className=' btn btn-primary'>Let's talk</a>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,103 @@
 import React from 'react'
 import './experience.css'
+import {TbCircleCheckFilled} from 'react-icons/tb'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Developpement</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+            <TbCircleCheckFilled className='experience__details-icons'></TbCircleCheckFilled>
+            <div>
+              <h4>HTML</h4>
+              <small className='text-light'>Basic</small>
+            </div>
+            </article>
+            <article className="experience__details">
+            <TbCircleCheckFilled className='experience__details-icons'></TbCircleCheckFilled>
+            <div>
+              <h4>CSS</h4>
+              <small className='text-light'>Basic</small>
+            </div>
+            </article>
+            <article className="experience__details">
+            <TbCircleCheckFilled className='experience__details-icons'></TbCircleCheckFilled>
+            <div>
+              <h4>JavaScript</h4>
+              <small className='text-light'>Basic</small>
+            </div>
+            </article>
+            <article className="experience__details">
+            <TbCircleCheckFilled className='experience__details-icons'></TbCircleCheckFilled>
+            <div>
+              <h4>React</h4>
+              <small className='text-light'>Basic</small>
+            </div>
+            </article>
+            <article className="experience__details">
+            <TbCircleCheckFilled className='experience__details-icons'></TbCircleCheckFilled>
+            <div>
+              <h4>React Native</h4>
+              <small className='text-light'>Basic</small>
+            </div>
+            </article>
+            <article className="experience__details">
+            <TbCircleCheckFilled className='experience__details-icons'></TbCircleCheckFilled>
+            <div>
+              <h4>Redux</h4>
+              <small className='text-light'>Basic</small>
+            </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRONT */}
+        <div className="experience__backend">
+        <h3>Backend Developpement</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+            <TbCircleCheckFilled className='experience__details-icons'></TbCircleCheckFilled>
+            <div>
+              <h4>Express</h4>
+              <small className='text-light'>Basic</small>
+            </div>
+            </article>
+            <article className="experience__details">
+            <TbCircleCheckFilled className='experience__details-icons'></TbCircleCheckFilled>
+            <div>
+              <h4>Node Js</h4>
+              <small className='text-light'>Basic</small>
+            </div>
+            </article>
+            {/* <article className="experience__details">
+            <TbCircleCheckFilled></TbCircleCheckFilled>
+            <h4>JavaScript</h4>
+            <small className='text-light'>Basic</small>
+            </article>
+            <article className="experience__details">
+            <TbCircleCheckFilled></TbCircleCheckFilled>
+            <h4>React</h4>
+            <small className='text-light'>Basic</small>
+            </article>
+            <article className="experience__details">
+            <TbCircleCheckFilled></TbCircleCheckFilled>
+            <h4>React Native</h4>
+            <small className='text-light'>Basic</small>
+            </article>
+            <article className="experience__details">
+            <TbCircleCheckFilled></TbCircleCheckFilled>
+            <h4>Redux</h4>
+            <small className='text-light'>Basic</small>
+            </article> */}
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
