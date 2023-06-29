d16b33d7d0169cc5aa2d29e368e8a127e8efda29
Ha_Tan_Luc <hatanluc0@outlook.fr>
2023-06-29 17:49:24 +0200

    Quatrième commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,21 +12,21 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-          <AiOutlineMail></AiOutlineMail>
+          <AiOutlineMail className='contact__option-icon'></AiOutlineMail>
           <h4>Email</h4>
           <h5>dummygator@gmail.com</h5>
           <a href="hatanluc0@gmail.com"> Send a Message</a>
           </article>
 
           <article className="contact__option">
-          <BsMessenger></BsMessenger>
+          <BsMessenger className='contact__option-icon'></BsMessenger>
           <h4>Messenger</h4>
           <h5>hatanluc</h5>
           <a href="https://www.facebook.com/"> Send a Message</a>
           </article>
 
           <article className="contact__option">
-          <AiOutlineMail></AiOutlineMail>
+          <AiOutlineMail className='contact__option-icon'></AiOutlineMail>
           <h4>Whatsapp</h4>
           <h5>+00 000 000</h5>
           <a href="https://www.whatsapp.com/?lang=fr_FR"> Send a Message</a>
@@ -35,7 +35,8 @@ const Contact = () => {
         <form action="">
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
-          <textarea name="message" placeholder='' rows="7"></textarea>
+          <textarea name="message" placeholder='Your message' rows="7"></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
       </div>
     </section>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import "./footer.css"
+import {AiFillFacebook} from 'react-icons/ai'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiFillTwitterCircle} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <section>Footer</section>
+    <footer>
+      <a href="#" className='footer__logo'>Ha Tan Luc</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      
+      <div className="footer__socials">
+        <a href="https://facebook.com"><AiFillFacebook></AiFillFacebook></a>
+        <a href="https://instagram.com"><AiOutlineInstagram></AiOutlineInstagram></a>
+        <a href="https://twitter.com"><AiFillTwitterCircle></AiFillTwitterCircle></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; HA Tutorials. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
